076692ac1d03c4fbcdc9ee170616550919690bf9
mur4ik18 <sasha.bazalev1@gmail.com>
2020-03-15 13:01:47 +0200

    add comments

--- run.py
+++ run.py
@@ -4,58 +4,54 @@ import time
 
 import youtube_dl
 
+# add webdriver
 driver = webdriver.Chrome('chromedriver')
+# open stackoverflow
 driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
-
-
+# Musick links list
 musicList=[]
-
-em = ''
-pas = '' 
-
-
-
+# our login
+em = ' '
+pas = ' ' 
 # click funcrion
 def CLICK(a,b):
     z = driver.find_element_by_xpath(a)
     time.sleep(b)
     z.click()
-
+# write function
 def tipe(a,b,c):
     email = driver.find_element_by_xpath(a)
     time.sleep(b)
     email.click()
     time.sleep(b)
     email.send_keys(c)
-
-
-
+# click
 CLICK('//*[@id="openid-buttons"]/button[1]',0.5)
-
 # sign in
-#CLICK('//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[2]', 0.5)
 tipe('//*[@id="identifierId"]',0.5 ,em)
 CLICK('//*[@id="identifierNext"]', 0.5)
-
-
+# sleep 2 second
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="password"]').click()
+# sleep 0.5 sec
 time.sleep(0.5)
-
 driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(pas)
-
-
 CLICK('//*[@id="passwordNext"]/span/span', 0.5)
+# sleep 5 second
 time.sleep(5)
+# google ligin complate
+# open youtube
 driver.get('https://music.youtube.com/')
-
-
+# sleep 2 sec
 time.sleep(2)
 CLICK('//*[@id="layout"]/ytmusic-nav-bar/div[2]/ytmusic-pivot-bar-renderer/ytmusic-pivot-bar-item-renderer[3]/yt-formatted-string', 0.5)
+# sleep 2 sec
 time.sleep(2)
+# open youtube playlist
 driver.get('https://music.youtube.com/playlist?list=LM')
+# sleep 4 sec
 time.sleep(4)
-
+# get all links in playlist
 for i in range(1 , 100):
     link = driver.find_element_by_xpath('//*[@id="contents"]/ytmusic-responsive-list-item-renderer['+ str(i) +']/div[2]/div[1]/yt-formatted-string/a')
     print(link.get_attribute("href"))
@@ -63,7 +59,7 @@ for i in range(1 , 100):
     time.sleep(1)
 
 
-
+# download format 
 ydl_opts = {'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -71,5 +67,6 @@ ydl_opts = {'format': 'bestaudio/best',
         'preferredquality': '192',
     }],
 }
+# start download
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(musicList)
